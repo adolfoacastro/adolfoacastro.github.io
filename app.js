24b477bde040e740a4de39ded9b8f5098b8226c8
Adolfo Castro <adolfoacastro@gmail.com>
2020-07-31 05:22:24 -0500

    Changed a lot of things

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 'use strict'
 
 console.log('Here\'s a hidden message');
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
